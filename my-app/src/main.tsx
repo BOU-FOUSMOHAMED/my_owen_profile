a33f8ae7e0eea64d93963be4089f6549d1a763b0
BOU-FOUS MOHAMED <boufousmohamed2005@gmail.com>
2026-09-14 20:28:15 +0200

    Delete my-app directory

--- my-app/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
-import "./styles.css";
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
